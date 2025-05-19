fcc8939479a38d25c63f4d97b27062dcf311a451
sumit.webnotics@gmail.com <sumit.webnotics@gmail.com>
2025-05-19 09:42:45 +0530

    1905

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from './assets/logos/logo.png';
 import donateIcon from './assets/logos/donate-icon.svg';
+import toggleMenu from './assets/toggle-menu.svg';
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   // Check if token exists in localStorage
   const token = localStorage.getItem('token');
 
@@ -16,7 +19,10 @@ function Header() {
             <img src={logo} alt="logo" className="header-logo-img" />
           </Link>
         </h1>
-        <nav>
+        <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+          <img src={toggleMenu} alt="menu" />
+        </button>
+        <nav className={`nav ${menuOpen ? 'open' : ''}`}>
           <Link to="/about">About</Link>
           <Link to="/faq">FAQ</Link>
           <Link to="/contact">Contact Us</Link>
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef , useEffect} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 import { QRCodeCanvas } from 'qrcode.react';
 import Header from './Header';
@@ -126,7 +126,7 @@ function Home() {
       );
       setFulllink(res.data.link);
       setLink(res.data.link);
-      
+
     } catch (err) {
       console.error('API error:', err.response || err.message);
       alert('Error creating secure link.');
@@ -147,18 +147,18 @@ function Home() {
     // Handle different fields based on the name attribute
     if (name === 'country') {
       setSelectedCountry(value);
-      setFulllink(link+"&lg="+value);
-    } 
+      setFulllink(link + "&lg=" + value);
+    }
     // Add more conditions here if you have other fields to handle
   };
 
   const handlePrintQRCode = () => {
 
-      window.print();
-  
-    
+    window.print();
+
+
   };
-  
+
 
   // Mark the QR code as ready when it's rendered
   useEffect(() => {
@@ -166,10 +166,10 @@ function Home() {
       setQrCodeReady(true);  // Set QR code as ready when the component is mounted
     }
   }, [fulllink]);
-  
-  
 
-  
+
+
+
 
 
   return (
@@ -200,33 +200,33 @@ function Home() {
             <img src={createIcon} alt="Create secure link" /> {loading ? 'Creating link...' : 'Create secure link'}
           </button>
           {link && (
-            <div >
-              <p >One-time link:</p>
+            <div className='link_section'>
+              <h2>One-time link:</h2>
               <div className="wrp_container">
                 <input
                   type="text"
                   value={fulllink}
+                  className='link-input'
                   readOnly
-                  
+
                 />
-                 <select
-        name="country"
-        value={selectedCountry}
-        onChange={handleChangeCountry}
-      >
-        {countries.map((country) => (
-          <option key={country.code} value={country.code}>
-            {country.flag} {country.name}
-          </option>
-        ))}
-      </select>
-                <button onClick={copyToClipboard} >
-                <FiCopy style={{ marginRight: '8px' }} />
-                Copy 
+                <select
+                  name="country"
+                  value={selectedCountry}
+                  onChange={handleChangeCountry}
+                >
+                  {countries.map((country) => (
+                    <option key={country.code} value={country.code}>
+                      ({country.flag}) {country.name}
+                    </option>
+                  ))}
+                </select>
+                <button className='copyBtn' onClick={copyToClipboard} >
+                  <FiCopy />
                 </button>
               </div>
-              
-              
+
+
               <div style={{ textAlign: 'center' }} id="qrCodeContainer">
                 {/* QR Code */}
                 <QRCodeCanvas
@@ -237,19 +237,15 @@ function Home() {
               </div>
 
               <div>
-             
-                <button style={{padding: '13px 15px',
-    background: '#0862F7',
-    borderRadius: '5px',
-    color: '#fff',
-    border: 'none'}} onClick={handlePrintQRCode}>
-                  Print QR Code
+
+                <button className='printBtn' onClick={handlePrintQRCode}>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="2"><path d="M18.353 14H19c.943 0 1.414 0 1.707-.293C21 13.414 21 12.943 21 12v-1c0-1.886 0-2.828-.586-3.414C19.828 7 18.886 7 17 7H7c-1.886 0-2.828 0-3.414.586C3 8.172 3 9.114 3 11v2c0 .471 0 .707.146.854C3.293 14 3.53 14 4 14h1.647"></path><path d="M6 20.306V12c0-.943 0-1.414.293-1.707C6.586 10 7.057 10 8 10h8c.943 0 1.414 0 1.707.293c.293.293.293.764.293 1.707v8.306c0 .317 0 .475-.104.55c-.104.075-.254.025-.554-.075l-2.184-.728c-.078-.026-.117-.04-.158-.04c-.04 0-.08.014-.158.04l-2.684.894c-.078.026-.117.04-.158.04c-.04 0-.08-.014-.158-.04l-2.684-.894c-.078-.026-.117-.04-.158-.04c-.04 0-.08.014-.158.04l-2.184.728c-.3.1-.45.15-.554.075C6 20.781 6 20.623 6 20.306ZM18 7V5.88c0-1.008 0-1.512-.196-1.897a1.8 1.8 0 0 0-.787-.787C16.632 3 16.128 3 15.12 3H8.88c-1.008 0-1.512 0-1.897.196a1.8 1.8 0 0 0-.787.787C6 4.368 6 4.872 6 5.88V7"></path><path stroke-linecap="round" d="M10 14h3m-3 3h4.5"></path></g></svg> Print QR Code
                 </button>
               </div>
 
 
               <div >
-             
+
               </div>
             </div>
           )}
@@ -301,8 +297,8 @@ function Home() {
           </div>
         </div>
       </section>
-    
-       {/* Advanced Features */ }
+
+      {/* Advanced Features */}
       <section className='advanced-section'>
         <div className='container'>
           <h3><img src={secureChat} alt="FeatuEnsure secure conversations with one-time linksres" /><span>Ensure secure conversations with one-time links</span></h3>
@@ -348,9 +344,9 @@ function Home() {
           <p><strong>Secure One-Time Access</strong></p>
           <p className='mb-0'>Our backend ensures that the secret can only be accessed once. After the secret is retrieved, both the encrypted secret and private key are deleted from the database, ensuring the data is no longer accessible.</p>
         </div>
-      </section>  
- 
- 
+      </section>
+
+
       <section className="Trusted-section" >
         <div className='container'>
           <h3>Trusted secure password sharing since 2020</h3>
@@ -363,6 +359,6 @@ function Home() {
       <Footer />
     </div>
   );
-} 
+}
 
 export default Home;
